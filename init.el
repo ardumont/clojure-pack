c54eece12a823a82b55541c5147cecd7e5ebfffb
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-02 12:46:49 +0100

    Means to override the help char in cider-repl (not working so commented)

--- init.el
+++ init.el
@@ -29,6 +29,9 @@
             (and (fboundp 'smartscan-mode) smartscan-mode (smartscan-mode -1))
             (local-set-key (kbd "M-RET") 'cider-doc)))
 
+;; help in repl with :
+;; (setq cider-repl-shortcut-dispatch-char ?\:)
+
 (add-hook 'cider-mode-hook
           (lambda ()
             (local-set-key (kbd "C-c n e b") 'cider-eval-buffer)
