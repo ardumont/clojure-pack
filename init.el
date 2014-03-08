a47273a928c0456bedc2ec35d71a7197f905e6a2
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-03-08 09:50:18 +0100

    Add some cider configuration

--- init.el
+++ init.el
@@ -68,12 +68,6 @@
 ;; You can hide the *nrepl-connection* and *nrepl-server* buffers from appearing in some buffer switching commands like switch-to-buffer(C-x b) like this:
 ;; (setq nrepl-hide-special-buffers t)
 
-;; When using switch-to-buffer, pressing SPC after the command will make the hidden buffers visible. They'll always be visible in list-buffers (C-x C-b).
-;;  You can control the TAB key behavior in the REPL via the cider-repl-tab-command variable. While the default command cider-repl-indent-and-complete-symbol
-;; should be an adequate choice for most users, it's very easy to switch to another command if you wish to. For instance if you'd like TAB to only indent
-;; (maybe because you're used to completing with M-TAB) use the following snippet:
-;; (setq cider-repl-tab-command 'indent-for-tab-command)
-
 ;; Prevent the auto-display of the REPL buffer in a separate window after connection is established:
 ;; (setq cider-repl-pop-to-buffer-on-connect nil)
 
@@ -84,39 +78,32 @@
 ;; (setq cider-repl-popup-stacktraces t)
 
 ;; To auto-select the error buffer when it's displayed:
-;; (setq cider-auto-select-error-buffer t)
+(setq cider-auto-select-error-buffer t)
 
 ;; The REPL buffer name has the format *cider-repl project-name*. Change the separator from space to something else by overriding nrepl-buffer-name-separator.
 ;; (setq nrepl-buffer-name-separator "-")
 
 ;; The REPL buffer name can also display the port on which the nREPL server is running. Buffer name will look like cider-repl project-name:port.
-;; (setq nrepl-buffer-name-show-port t)
+(setq nrepl-buffer-name-show-port t)
 
 ;; Make C-c C-z switch to the CIDER REPL buffer in the current window:
-;; (setq cider-repl-display-in-current-window t)
+(setq cider-repl-display-in-current-window t)
 
-;;     Limit the number of items of each collection the printer will print to 100:
-;; (setq cider-repl-print-length 100) ; the default is nil, no limit
+;; Limit the number of items of each collection the printer will print to 100:
+(setq cider-repl-print-length 100) ; the default is nil, no limit
 
-;;     Prevent C-c C-k from prompting to save the file corresponding to the buffer being loaded, if it's modified:
-;; (setq cider-prompt-save-file-on-load nil)
+;; Prevent C-c C-k from prompting to save the file corresponding to the buffer being loaded, if it's modified:
+(setq cider-prompt-save-file-on-load t)
 
-;;     Change the result prefix for REPL evaluation (by default there's no prefix):
-;; (set cider-repl-result-prefix ";; => ")
-
-;; And here's the result of that change:
-
-;; user> (+ 1 2)
-;; ;; => 3
+;; Change the result prefix for REPL evaluation (by default there's no prefix):
+(setq cider-repl-result-prefix ";; ")
 
 ;; Change the result prefix for interactive evaluation (by default it's =>):
 ;; (set cider-interactive-eval-result-prefix ";; => ")
 
-;; To remove the prefix altogether just set it to an empty string("").
-
 ;; Normally code you input in the REPL is font-locked with cider-repl-input-face (after you press RET) and results are font-locked with cider-repl-output-face.
 ;; If you want them to be font-locked as in clojure-mode use the following:
-;; (setq cider-repl-use-clojure-font-lock t)
+(setq cider-repl-use-clojure-font-lock t)
 
 ;; You can control the C-c C-z key behavior of switching to the REPL buffer with the cider-switch-to-repl-command variable.
 ;; While the default command cider-switch-to-relevant-repl-buffer should be an adequate choice for most users, cider-switch-to-current-repl-buffer
